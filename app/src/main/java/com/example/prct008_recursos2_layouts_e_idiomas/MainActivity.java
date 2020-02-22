c44e3e0463b2f8d848417cdf9813734149f1fca0
Romulo Bianchin <romulobianchin986@gmail.com>
2020-02-22 22:55:01 +0000

    Start Commit

---
+++ app/src/main/java/com/example/prct008_recursos2_layouts_e_idiomas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.prct008_recursos2_layouts_e_idiomas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
